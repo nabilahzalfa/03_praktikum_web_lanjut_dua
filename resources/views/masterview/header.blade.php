aa8a6f0e95db79b734dc08abf3454c77bfbb6556
nabilahzalfa <nabilah.zalfa26@gmail.com>
2022-03-03 19:14:29 +0700

    revisi praktikum 2

--- resources/views/masterview/header.blade.php
+++ resources/views/masterview/header.blade.php
@@ -1,23 +1,23 @@
-<div class="tm-top-header">
-        <div class="container">
+    <div class="tm-top-header">
+      <div class="container">
         <div class="row">
-            <div class="tm-top-header-inner">
+          <div class="tm-top-header-inner">
             <div class="tm-logo-container">
-                <img src="asset/img/logo.png" alt="Logo" class="tm-site-logo">
-                <h1 class="tm-site-name tm-handwriting-font">AADA Cafe</h1>
+              <img src="asset/img/logo.png" alt="Logo" class="tm-site-logo">
+              <h1 class="tm-site-name tm-handwriting-font">AADA Cafe</h1>
             </div>
             <div class="mobile-menu-icon">
-                <i class="fa fa-bars"></i>
+              <i class="fa fa-bars"></i>
             </div>
-                <nav class="tm-nav">
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/today-special">Today Special</a></li>
-                        <li><a href="/menu">Menu</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </nav>   
-            </div>           
+            <nav class="tm-nav">
+              <ul>
+                <li><a href="/" class="{{ ($title == "AADA Cafe - Home" )? "active" : "" }}">Home</a></li>
+                <li><a href="/today-special" class="{{ ($title == "AADA Cafe - Today Special" )? "active" : "" }}">Today Special</a></li>
+                <li><a href="/menu" class="{{ ($title == "AADA Cafe - Menu" )? "active" : "" }}">Menu</a></li>
+                <li><a href="/contact" class="{{ ($title == "AADA Cafe - Contact" )? "active" : "" }}">Contact</a></li>
+              </ul>
+            </nav>   
+          </div>           
         </div>    
-        </div>
+      </div>
     </div>
